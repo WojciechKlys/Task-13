e81a231b78627d2b8df95a1cc242aae744fab7fb
Wojtek Klys <wojciechklys@gmail.com>
2021-02-15 18:00:16 +0100

    task13

---
+++ js/button.js
@@ -0,0 +1,9 @@
+document.querySelector(".theme-button").addEventListener("click", () => {
+    const container = document.querySelector(".dashboard-container");
+    container.classList.toggle("theme-night");
+});
+
+
+
+
+
